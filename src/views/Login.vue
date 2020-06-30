f53e82ba9548578d748f8c74f6308c489494f76c
盛盛盛 <1445089856@qq.com>
2020-06-30 13:18:20 +0800

    'jsonp部署修改'

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,9 +64,9 @@ export default {
       }
       this.loading = true
       this.btnDisabled = true
-      this.$jsonp('http://localhost:22390/api/User/Login', { UserName: this.user.Account, Password: this.user.Password }).then(res => {
-        // this.$jsonp('http://152.136.139.149:8091/api/User/Login').then(res => {
-        // this.$jsonp('http://10.10.42.81:8090/api/User/Login').then(res => {
+      // this.$jsonp('http://localhost:22390/api/User/Login', { UserName: this.user.Account, Password: this.user.Password }).then(res => {
+      // this.$jsonp('http://152.136.139.149:8091/api/User/Login', { UserName: this.user.Account, Password: this.user.Password }).then(res => {
+      this.$jsonp('http://10.10.42.81:8090/api/User/Login', { UserName: this.user.Account, Password: this.user.Password }).then(res => {
         window.sessionStorage.setItem('token', res)
         const tokenParse = jwtDecode(window.sessionStorage.token)
         console.log(tokenParse)
